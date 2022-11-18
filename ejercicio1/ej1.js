674a49cf0134bd8359dc776505279787b6e283e9
Dmardomcad <daniel.mariscal@iesrafaelalberti.es>
2022-11-18 08:46:42 +0100

    Subida

---
+++ ejercicio1/ej1.js
@@ -0,0 +1,57 @@
+// Creamos un array vacío donde se almacenarán los jugadores
+let jugadores = []
+
+// Creamos el objeto jugador con sus propiedades y métodos
+const Jugador = {
+    Nombre: "Daniel",
+    Apellidos: "Mariscal",
+    Nivel: 1,
+    Puntuacion: 1,
+    ganarPuntuacion: function(){
+        console.log("Has matado un zombi");
+        this.Puntuacion++
+        if (this.Puntuacion>10){
+            this.Nivel++
+            console.log(`Has subido de nivel, tu nivel es ahora ${this.Nivel}`);
+            this.Puntuacion = 1
+        }
+    } ,
+    restarPuntuacion: function(){
+        console.log("Un zombie te ha alcanzado");
+        this.Puntuacion--
+        if(this.Puntuacion){
+            this.Nivel--
+            if (this.Puntuacion < 0){
+                this.resetearNivel()
+            }
+        }
+        
+    } ,
+    resetearNivel: function(){
+        if (this.Puntuacion===0){
+            alert(`El jugador ${this.Nombre} + ${this.Apellidos} ha muerto!`)
+            this.Nivel=0
+        }
+    }
+}
+
+console.log(Jugador);
+console.log(Jugador.ganarPuntuacion());
+console.log(Jugador);
+console.log(Jugador.ganarPuntuacion());
+console.log(Jugador);
+console.log(Jugador);
+console.log(Jugador.ganarPuntuacion());
+console.log(Jugador);
+console.log(Jugador.ganarPuntuacion());
+console.log(Jugador);
+console.log(Jugador);
+console.log(Jugador.ganarPuntuacion());
+console.log(Jugador);
+console.log(Jugador.ganarPuntuacion());
+console.log(Jugador);
+console.log(Jugador);
+console.log(Jugador.ganarPuntuacion());
+console.log(Jugador);
+console.log(Jugador.ganarPuntuacion());
+console.log(Jugador);
---
+++ ejercicio2/ej2.js
@@ -0,0 +1,26 @@
+// Creamos una función para validar si un año es bisiesto
+let n = 0
+let m = 0
+function isLeapYear(n,m){
+
+    n = Math.round(n)
+    m = Math.round(m)
+
+    if (isNaN(n) || isNaN(m)){
+        console.log(`Entrada no válida, ${n} o ${m} No es un número`);
+    
+    }
+    else if (n < 2001){
+        console.log(`Entrada no válida, ${n} es menor que 2001`);
+    }
+    else if (m > 2500){
+        console.log(`Entrada no válida, ${n} o ${m} es mayor que 2500`);
+    }
+    else if (n >= 2001 && n <=2500){
+     
+    }
+
+
+}
+
+console.log(isLeapYear(2532, 2432.3));
